e9b35883101bbcc6825c48c6c2221e8e6851ff5a
Charmink <46817577+Charmink@users.noreply.github.com>
2020-02-20 23:59:52 +0300

    10 commit fix

--- Api.py
+++ Api.py
@@ -19,6 +19,7 @@ class MyWidget(QMainWindow):
         self.type = False
         self.pushButton.clicked.connect(self.search)
         self.pushButton_2.clicked.connect(self.vipe)
+        self.checkBox.clicked.connect(self.search)
 
     def keyPressEvent(self, event):
         global map_params
